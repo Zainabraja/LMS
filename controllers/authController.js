9f4872b68fbd21cccae0fd778c3da39ccf19ddcc
Zainabraja <zainabraja53@gmail.com>
2022-03-17 21:53:38 +0530

    commit

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,4 @@
-const User = require("../models/User");
+const User = require("../models/user");
 const Task = require("../models/task")
 const jwt = require('jsonwebtoken');
 const cloudinary = require('cloudinary').v2;
